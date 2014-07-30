44264af58b1e3d743e918fedbec20a8756fceb6b
Dylan Peerenboom & Tanner Steward <student@epicodus.com>
2014-07-30 10:53:48 -0700

    added .clear, .all, all specs passed

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,12 +1,20 @@
+require 'phone'
+require 'email'
+require 'address'
+
 class Contacts
 
 @@all_contacts = []
 
   def initialize (name)
     @name = name
-    @email = []
-    @address = []
-    @phone = []
+    @emails = []
+    @addresses = []
+    @phones = []
+  end
+
+  def Contacts.clear
+    @@all_contacts = []
   end
 
   def Contacts.all
@@ -18,18 +26,38 @@ class Contacts
   end
 
   def all_info
-    '#{@email}\n #{@address}\n #{@phone}'
+    "#{@name}#{@email}\n#{@address}\n#{@phone}"
   end
 
-  def add_phone
-    new_phone = Phone.new(phone)
-    @phone = phone
+  def name
+    @name
   end
 
-  def add_email
-    new_email = Email.new(email)
-    @email = email
+  def phones
+    @phones
   end
 
+  def add_phone(user_phone)
+    new_phone = Phone.new(user_phone)
+    @phones << new_phone
+  end
+
+  def emails
+    @emails
+  end
+
+  def add_email(user_email)
+    new_email = Email.new(user_email)
+    @emails << new_email
+  end
+
+  def addresses
+    @addresses
+  end
+
+  def add_address(user_address)
+    new_address = Address.new(user_address)
+    @addresses << new_address
+  end
 end
 
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -2,13 +2,58 @@ require 'Contacts'
 require 'rspec'
 
 describe Contacts do
+  before do
+    Contacts.clear
+  end
   it 'is initialized with a name' do
     test_contact = Contacts.new('Bob')
     expect(test_contact).to be_an_instance_of Contacts
   end
 
+  describe '.clear' do
+    it 'empties all the saved contacts' do
+      Contacts.new('joanne').save
+      Contacts.clear
+      Contacts.all.should eq []
+    end
+  end
+
   describe '#save' do
     it 'adds a contact to the array of saved contacts' do
+      test_contact = Contacts.new('Bob')
+      ruby_contact = Contacts.new('Ruby')
+      test_contact.save
+      ruby_contact.save
+      expect(Contacts.all).to eq [test_contact, ruby_contact]
+    end
+  end
+  describe 'add_phone' do
+    it 'it adds a phone number to a contact' do
+      test_contact = Contacts.new('Bob')
+      test_contact.save
+      test_contact.add_phone(12312312)
+      expect(test_contact.phones[0].phone).to eq 12312312
+    end
+  end
+  describe 'add_email' do
+    it 'it adds an email to a contact' do
+      test_contact = Contacts.new('Bob')
+      test_contact.save
+      test_contact.add_email('bob@gamil')
+      expect(test_contact.emails[0].email).to eq 'bob@gamil'
+    end
+  end
+
+  describe 'add_address' do
+    it 'adds an address to a contact' do
+      test_contact = Contacts.new('Bob')
+      test_contact.save
+      test_contact.add_address('123 st.')
+      expect(test_contact.addresses[0].address).to eq '123 st.'
+    end
+  end
+  describe '.all' do
+    it 'lists the contacts from the array of saved contacts' do
       test_contact = Contacts.new('Bob')
       ruby_contact = Contacts.new('Ruby')
       test_contact.save
